477046dd68b8c20c9e2d4d2f1f2b336d83912bb4
conrad2001 <43147476+conrad2001@users.noreply.github.com>
2019-10-10 22:55:24 -0700

    Add files via upload

--- millis.c
+++ millis.c
@@ -9,6 +9,7 @@
 #include "msp.h"
 #include "millis.h"
 #include "stdint.h"
+#include "ISR.h"
 
 uint64_t _millis;
 
@@ -16,33 +17,35 @@ uint64_t _millis;
 uint64_t millis(){
     static i;
     if (!i){
-        TIMER_A0->CCR[0] = 33;   //2s @ 32.768kHz
-        TIMER_A0->CCTL[0] = TIMER_A_CCTLN_CCIE; //enable interrupt, compare mode
+        //fix ACLK at 32.768kHz
+        CS->KEY = CS_KEY_VAL;           // Unlock CS module for register access
+        CS->CTL1 &= ~( CS_CTL1_SELA__LFXTCLK | CS_CTL1_SELA__VLOCLK );  //fix ACLK to use REFOCLK
+        CS->CTL1 |= CS_CTL1_SELA__REFOCLK;
+        CS->KEY = 0;                    // Lock CS module from unintended accesses
+        //check ACLK divider and match timing to 1ms for different dividing factors
+        switch(CS->CTL1 & 1 << 24){
+        case CS_CTL1_DIVA__1: TIMER_A3->CCR[0]      = 31;   //1ms @ 32.768kHz
+        case CS_CTL1_DIVA__2: TIMER_A3->CCR[0]      = 62;
+        case CS_CTL1_DIVA__4: TIMER_A3->CCR[0]      = 124;
+        case CS_CTL1_DIVA__8: TIMER_A3->CCR[0]      = 248;
+        case CS_CTL1_DIVA__16: TIMER_A3->CCR[0]     = 496;
+        case CS_CTL1_DIVA__32: TIMER_A3->CCR[0]     = 992;
+        case CS_CTL1_DIVA__64: TIMER_A3->CCR[0]     = 1984;
+        case CS_CTL1_DIVA__128: TIMER_A3->CCR[0]    = 3968;
+        }
+
+        TIMER_A3->CCTL[0] = TIMER_A_CCTLN_CCIE; //enable interrupt, compare mode
         //select ACLK | select continuous mode
-        TIMER_A0->CTL = TIMER_A_CTL_SSEL__ACLK | TIMER_A_CTL_MC__CONTINUOUS ;
-        ISER_SEL(TA0_0_IRQn);       //enable CCR0 timer A0 interrupt in NVIC
+        TIMER_A3->CTL = TIMER_A_CTL_SSEL__ACLK | TIMER_A_CTL_MC__CONTINUOUS ;
+        ISER_SEL(TA3_0_IRQn);       //enable CCR0 timer A0 interrupt in NVIC
         __enable_irq();     //enable interrupt globally
         i++;
     }
         return _millis;
 }
 
-void TA0_0_IRQHandler(){
-    /*TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;   //clear interrupt flag
-    P1->OUT ^= BIT0;
-    P1->OUT ^= BIT0;*/
-    //CCR0 ISR, turn LED on and off
-    /*static i;
-    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;   //clear interrupt flag
-    if( i < 1) {
-        P1->OUT &= ~BIT0;
-        i++;
-    }
-    else {
-        P1->OUT |= BIT0;
-        i = 0;
-    }*/
-    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;   //clear interrupt flag
+void TA3_0_IRQHandler(){
+    TIMER_A3->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;   //clear interrupt flag
     _millis++;
-    TIMER_A0->CCR[0] += 33;  //add 0.1 ms
+    TIMER_A3->CCR[0] += 32;  //add 0.1 ms @ 32.768kHz
 }
